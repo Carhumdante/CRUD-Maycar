69a5a64e2ab80e7659f0c1c1032b1038c19c40c3
Carhumdante <carhumdante@gmail.com>
2022-07-21 08:54:50 -0500

    Update clientes.php

--- vistas/clientes.php
+++ vistas/clientes.php
@@ -3,17 +3,17 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'PPY3PFCXSNK6LNJZ7GRQIRXEWYYMJCPC@10.0.0.199/workgroup2/presta/api/customers?output_format=JSON&display=[id,firstname,lastname,email]',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Cookie: PrestaShop-b495097bf03a2b246a17e22e09a4dd34=def50200597a9629fd5d63f742819b8bda4d9ffe59388d9039c2245876383d97122e2f3f94e7a9906556637a650dff83eff777269856c4227e478c71382083a1954e594be55a8c2261f7e778b872791a7d528ab925d172c7e80db7b5d39597e480a5007cc72f6a76946bdd1bd91615e14e30ba67f740af465ab36873d4cda0714d8f99a045d36442981a95db9be0e2d789c2e03966a39d493741001a5b412eefab128541f22d418471241d04592e44fe9b74d468fbfaaa0e8eaedacaa3ee27f6787902db862f8ca329f27249f14c76a94de73ada780abbff41'
-  ),
+    CURLOPT_URL => 'PPY3PFCXSNK6LNJZ7GRQIRXEWYYMJCPC@10.0.0.199/workgroup2/presta/api/customers?output_format=JSON&display=[id,firstname,lastname,email]',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Cookie: PrestaShop-b495097bf03a2b246a17e22e09a4dd34=def50200597a9629fd5d63f742819b8bda4d9ffe59388d9039c2245876383d97122e2f3f94e7a9906556637a650dff83eff777269856c4227e478c71382083a1954e594be55a8c2261f7e778b872791a7d528ab925d172c7e80db7b5d39597e480a5007cc72f6a76946bdd1bd91615e14e30ba67f740af465ab36873d4cda0714d8f99a045d36442981a95db9be0e2d789c2e03966a39d493741001a5b412eefab128541f22d418471241d04592e44fe9b74d468fbfaaa0e8eaedacaa3ee27f6787902db862f8ca329f27249f14c76a94de73ada780abbff41'
+    ),
 ));
 
 $response = curl_exec($curl);
@@ -50,23 +50,29 @@ $x = $response;
                         </div>
                     </div>
                 </div>
-                    <form action="update.php" method="POST" id="myform">
+                <form action="update.php" method="POST" id="myform">
 
-                        <table class="table table-bordered">
-                        <?php for($i=0;$i<5;$i++) {?>
-                        <?php foreach ($response as $list) {
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Id</th>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>Email</th>
+                        </tr>
+                        <?php for ($i = 0; $i < 2; $i++) { ?>
+                            <?php foreach ($response as $list) {
 
-                            ?>  
-                            <tr>
-                            <td><?php echo $list[$i]->id; ?></td>
-                            <td><?php echo $list[$i]->firstname; ?></td>
-                            <td><?php echo $list[$i]->lastname; ?></td>
-                            <td><?php echo $list[$i]->email; ?></td><
-                            </tr>                
-                            <?php
-                            } ?>
-                            <?php }?>
-                    </form>
+                            ?>
+                                <tr>
+                                    <td><?php echo $list[$i]->id; ?></td>
+                                    <td><?php echo $list[$i]->firstname; ?></td>
+                                    <td><?php echo $list[$i]->lastname; ?></td>
+                                    <td><?php echo $list[$i]->email; ?></td>
+                                    < </tr>
+                                    <?php
+                                } ?>
+                                <?php } ?>
+                </form>
             </div>
         </div>
     </div>
